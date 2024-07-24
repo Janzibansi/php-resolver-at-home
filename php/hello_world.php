46f1f915b7b37b7312d18165d3fe5fdeefa6fa48
Janzibansi <129806928+Janzibansi@users.noreply.github.com>
2024-07-24 15:04:59 +0200

    Add files via upload

--- php/hello_world.php
+++ php/hello_world.php
@@ -1,1 +1,13 @@
 
+<?php
+print '<p>bakflhfaLHFDSLFKHASLKHFLÄASIghlkhingus</p>'; 
+
+
+echo "<br><hr><br>";
+
+
+
+// zurück-Button
+
+echo '<button class="back-button" onclick="history.back()">Back</button>';
+?>
